6db61b581846113dc2107d3d6c832e82ba23b70f
Yasmine Ghanem <74925701+yasmineghanem@users.noreply.github.com>
2024-03-04 13:22:07 +0200

    generate.py

--- Lab 3/generate.py
+++ Lab 3/generate.py
@@ -2,22 +2,22 @@
 import argparse
 import random
 
-def generate_test_file(size,filename):
+def generate_test_file(array_size,filename):
     with open(filename, "w") as file:
-        for _ in range(size):
+        for _ in range(array_size):
             file.write(str(round(random.uniform(1, 10), 1))+ "\n")
 
 def main():
     parser = argparse.ArgumentParser(description="Generate input file.")
-    parser.add_argument("size", type=int, help="number of elements")
+    parser.add_argument("array_size", type=int, help="number of elements")
     parser.add_argument("filename", type=str, help="Output filename")
     args = parser.parse_args()
 
-    generate_test_file(args.size,args.filename)
+    generate_test_file(args.array_size,args.filename)
 
 if __name__ == "__main__":
     main()
 
 
-# python ./tests/generate.py num_test_cases rows columns filename
-#  python ./tests/generate.py 1 4 3 ./tests/test_4_3.txt
+# python ./tests/generate.py array_array_size filename
+#  python ./tests/generate.py 10 ./tests/test_10.txt
